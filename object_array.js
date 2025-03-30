2cbb54abb65edbef5954a460ce34236edd2f6ee4
Deepak Kumar <dk45003835@gmail,com>
2025-03-30 22:20:23 +0530

    update array

--- object_array.js
+++ object_array.js
@@ -1,2 +1,6 @@
 //Problem: Create an array containging diffrent type of teas.
-let tea = ['green tea', 'black tea', 'oolong tea', 'white tea', 'milk tea'];
+let tea = ['green tea', 'black tea', 'oolong tea', 'white tea', 'milk tea']
+console.log(tea);
+//Problem: Add "Chamomile Tea" to the exixting list of teas.
+tea.push('Chamomile Tea');
+console.log(tea);
